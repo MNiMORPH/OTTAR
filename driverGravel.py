b346e0c0e29d9a1683e3970b60db0eca6eb925db
Andy Wickert <andy@northernwidget.com>
2021-11-27 10:07:25 +0100

    tlw --> rw; internal plot

--- driverGravel.py
+++ driverGravel.py
@@ -1,21 +1,16 @@
-import riverwidth as rw
+import riverwidth
 from matplotlib import pyplot as plt
 import importlib
-importlib.reload(rw)
+importlib.reload(riverwidth)
 
-tlw = rw.WidthNoncohesiveBanks(h_banks=4., S=1E-2, D=2E-2, Q0=5., k_n=1E-3)
+rw = riverwidth.WidthNoncohesiveBanks(h_banks=4., S=1E-2, D=2E-2, Q0=5., k_n=1E-3)
 
 import numpy as np
 t = np.arange(0,10000*120,10000)
 Q = 10.*np.ones(len(t))
 
-tlw.initialize(t,Q)
-tlw.run()
-tlw.finalize()
+rw.initialize(t,Q)
+rw.run()
+rw.finalize()
+rw.plot()
 
-plt.figure()
-plt.plot(t/(60*60*24.), tlw.b)
-plt.xlabel('Days')
-plt.ylabel('Width [m]')
-plt.tight_layout()
-plt.show()
