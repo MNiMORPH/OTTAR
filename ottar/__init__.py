03afc8807dbb8a385eb7dcd342e256371ffcef7f
Andy Wickert <andy@northernwidget.com>
2022-04-26 11:28:20 +0200

    Import only RiverWidth and FlowDepthDoubleManning

--- ottar/__init__.py
+++ ottar/__init__.py
@@ -1,9 +1,5 @@
 # module will import as ottar (folder name)
 # This sets which objects, functions, etc. are exposed
-# e.g., 
-# from .ottar import RiverWidth
-# I will just expose all of them until I start to have too many
-# or want to restrict the UI
-# (note the . relative import)
-from .ottar import *
+from .ottar import FlowDepthDoubleManning
+from .ottar import RiverWidth
 
