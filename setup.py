e15a7c9935011494f39eeb1852712c9f57641039
Andy Wickert <andy@northernwidget.com>
2022-04-26 11:39:46 +0200

    Down to v0.2.0: space for PyPI testing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OTTAR",
-    version="1.0.0",
+    version="0.2.0",
     author="Andrew D. Wickert",
     author_email="awickert@umn.edu",
     description="Ode To Transient (Ancho de los) Rivers: Transient evolution of river-channel width",
