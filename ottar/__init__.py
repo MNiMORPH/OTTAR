4d1a7d8e3c1b00dca0a37e1c1f570eca66ef8b0a
awickert <andy@northernwidget.com>
2023-07-18 15:23:53 +0200

    Import version number.

--- ottar/__init__.py
+++ ottar/__init__.py
@@ -1,5 +1,6 @@
 # module will import as ottar (folder name)
 # This sets which objects, functions, etc. are exposed
+from ._version import __version__
 from .ottar import FlowDepthDoubleManning
 from .ottar import RiverWidth
 
