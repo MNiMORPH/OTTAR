4e9677b4859a32249dfed5854a0ceaf87e32ee8d
awickert <andy@northernwidget.com>
2024-01-08 19:17:40 -0500

    Create updated YAML config with Minnesota River example.

---
+++ examples/MinnesotaJordan/MinnesotaJordan.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+"""
+This is meant as a schematic example, using data from the Minnesota River at
+Jordan (MN) stream gauge. Parameters are not calibrated to data, and are not
+intended to fit real river-width measurements.
+"""
+
+import ottar
+import pandas as pd
+from matplotlib import pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
+
+cfs_to_m3s = 0.0283168466
+seconds_in_day = float(24 * 60 * 60)
+
+streamflowData = pd.read_csv('MN_Jordan_daily.csv')
+streamflowData['Timestamp'] = streamflowData['Timestamp'].astype('datetime64[ns]')
+streamflowData = streamflowData.sort_values('Timestamp')
+
+rw = ottar.RiverWidth(h_banks=6., S=1E-4, tau_crit=5, k_d=2E-6, k_E=0.4,
+                                b0=65., f_stickiness=1E-2,
+                                k_n_noncohesive=2E-5, D=0.0005
+                                )
+
+t = streamflowData['Timestamp']
+Q = streamflowData['Discharge (cfs)'] * cfs_to_m3s
+
+#t = streamflowData['Timestamp'][:4000]
+#Q = streamflowData['Discharge (cfs)'][:4000] * cfs_to_m3s
+
+rw.initialize_flow_calculations(0.031, 180., 1.5)
+
+rw.initialize_timeseries(t, Q)
+rw.run()
+rw.finalize()
+
+rw.plotQb()
+
