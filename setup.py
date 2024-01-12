455c851af5dbe80781690f6b6f398770937e228e
awickert <andy@northernwidget.com>
2024-01-11 22:54:21 -0500

    Increment version 0.5.0 --> 0.6.0 (belatedly); update DOI.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OTTAR",
-    version="0.5.0",
+    version="0.6.0",
     author="Andrew D. Wickert",
     author_email="awickert@umn.edu",
     description="Ode To Transient Ancho de los Rivers: Transient evolution of river-channel width",
